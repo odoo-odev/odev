ad8a519f90a36b9aef6469279a8c10659410651e
nda-odoo <nda@odoo.com>
2021-07-06 08:57:55 +0200

    [IMP] add sql queries to clean script

--- src/scripts/clean.py
+++ src/scripts/clean.py
@@ -13,18 +13,22 @@ Makes a local Odoo database suitable for development:
 - Disables automated and scheduled actions
 - Disables mails
 - Set credentials for Administrator user to admin:admin
-- Set password for all other users to odoo
-- Extend database validity to December 2050
+- Set password for first 50 users to odoo
+- Extend database validity to December 2050 and remove enterprise code
+- Set report.url and web.base.url to http://localhost:8069
 """
 
     queries = [
         "UPDATE res_users SET login='admin',password='admin' WHERE id IN (SELECT id FROM res_users WHERE active='True' ORDER BY id ASC LIMIT 1)",
-        "UPDATE res_users SET password='odoo' WHERE login != 'admin' AND password IS NOT NULL",
+        "UPDATE res_users SET password='odoo' WHERE login != 'admin' AND password IS NOT NULL AND id IN (SELECT id FROM res_users WHERE active='True' ORDER BY id ASC LIMIT 50)",
         "UPDATE ir_config_parameter SET value='2050-12-12' WHERE key='database.expiration_date'",
+        "DELETE FROM ir_config_parameter where key ='database.enterprise_code'",
         "UPDATE ir_config_parameter SET value='http://localhost:8069' WHERE key='web.base.url'",
+        "UPDATE ir_config_parameter SET value='http://localhost:8069' WHERE key='report.url'",
         "UPDATE ir_cron SET active='False'",
         "DELETE FROM fetchmail_server",
         "DELETE FROM ir_mail_server",
+        # "UPDATE auth_oauth_provider SET enabled = false",
     ]
 
     def run(self, database, options):
