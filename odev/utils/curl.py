dba0090ba61f9ed66ea1169b3d1449d1e1ed0ab7
avs <avs@odoo.com>
2021-11-04 08:59:53 +0100

    [IMP] Capture SIGINT and SIGNUM in curl subprocess

--- odev/utils/curl.py
+++ odev/utils/curl.py
@@ -2,6 +2,8 @@
 
 import os
 
+from odev.utils.signal import capture_signals
+
 
 # TODO: move to requests library
 def curl(url, *args, include_response_headers=True, follow_redirects=True, silent=True):
@@ -13,6 +15,7 @@ def curl(url, *args, include_response_headers=True, follow_redirects=True, silen
     if silent:
         options.append('-s')
     compiled_args = ' '.join(options + list(args))
-    cmdline = f'curl {compiled_args} `{url}`'
-    stream = os.popen(cmdline)
+    cmdline = f'curl {compiled_args} "{url}"'
+    with capture_signals():
+        stream = os.popen(cmdline)
     return stream.read().strip()
