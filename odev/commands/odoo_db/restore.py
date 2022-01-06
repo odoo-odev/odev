7ef40ac68594d2e9bbb2f627de122092c0c52a47
Marten (msc) <marten.schlueter@gmail.com>
2022-01-06 15:21:19 +0100

    [IMP] `restore`: pipe extract
    The temp partitions size can be limited. The original solution to extract zipped dumps was using pythons `tempfile` lib which by standard writes to the temp partition. Especially big databases are able to fill the temp partition on many systems.
    
    Why use the filesystem in the first place?
    `dump.sql` is directly piped to `psql`. The filestore is handled according to the users choice, also w/o `tempfile`.

--- odev/commands/odoo_db/restore.py
+++ odev/commands/odoo_db/restore.py
@@ -6,7 +6,6 @@ import shutil
 import subprocess
 from argparse import Namespace
 from pathlib import Path
-from tempfile import TemporaryDirectory
 from zipfile import ZipFile
 
 from odev.structures import commands
@@ -18,6 +17,26 @@ from odev.exceptions import RunningOdooDatabase, CommandAborted
 
 _logger = logging.getLogger(__name__)
 
+def pg_subprocess(fnc):
+    def wrapper(*args):
+        database = args[0]
+        _logger.info(f'Importing SQL data to database {database}')
+        commandline = fnc(*args)
+        with capture_signals():
+            subprocess.run(commandline, shell=True, check=True, stdout=subprocess.DEVNULL)
+    return wrapper
+
+@pg_subprocess
+def pg_restore(database, dump_path):
+    return ['pg_restore', *('-d', database), dump_path]
+
+@pg_subprocess
+def psql_load(database, sql_file):
+    return f'psql "{database}" < "{sql_file}"'
+
+@pg_subprocess
+def psql_pipe(database, cmd):
+    return f'{cmd} | psql "{database}"'
 
 class RestoreCommand(commands.LocalDatabaseCommand):
     '''
@@ -77,60 +96,23 @@ class RestoreCommand(commands.LocalDatabaseCommand):
         _, ext = os.path.splitext(tail)
 
         if not ext:
-            raise ValueError(f'File `{self.dump_path}` has no extension, couldn\'t guess what to do...')
-        elif ext not in {'.dump', '.zip', '.sql', '.gz'}:
-            raise ValueError(f'Unrecognized extension `{ext}` for file {self.dump_path}')
+            raise ValueError(f'File `{self.dump_path}` has no extension, couldn\'t guess what to do...')       
 
         _logger.info(f'Restoring dump file `{self.dump_path}` to database {self.database}')
         _logger.warning('This may take a while, please be patient...')
 
-        def pg_subprocess(commandline):
-            nonlocal self
-            _logger.info(f'Importing SQL data to database {self.database}')
-
-            with capture_signals():
-                subprocess.run(commandline, shell=True, check=True, stdout=subprocess.DEVNULL)
-
-        def pg_restore(database, dump_path):
-            pg_subprocess(['pg_restore', *('-d', database), dump_path])
-
-        def psql_load(database, sql_file):
-            pg_subprocess(f'psql "{database}" < "{sql_file}"')
-
-        def psql_pipe(database, cmd):
-            pg_subprocess(f'{cmd} | psql "{database}"')
-
-        if ext == '.dump':
-            pg_restore(self.database, self.dump_path)
-        elif ext == '.sql':
-            psql_load(self.database, self.dump_path)
-        elif ext == '.gz':
-            cmd = f"zcat {self.dump_path}"
-            psql_pipe(self.database, cmd)
-        elif ext == '.zip':
-            with TemporaryDirectory() as tempdir, ZipFile(self.dump_path, 'r') as zipref:
-                zipref.extractall(tempdir)
-
-                tmp_sql_path = os.path.join(tempdir, 'dump.sql')
-                if not os.path.isfile(tmp_sql_path):
-                    raise Exception(f'Could not extract `dump.sql` from {self.dump_path}')
-
-                tmp_filestore_path = os.path.join(tempdir, 'filestore')
-                if os.path.isdir(tmp_filestore_path):
-                    filestores_root = Path.home() / '.local/share/Odoo/filestore'
-                    filestore_path = str(filestores_root / self.database)
-                    _logger.info(f'Filestore detected, installing to {filestore_path}')
-
-                    if os.path.isdir(filestore_path):
-                        _logger.warning(f'A filestore already exists at `{filestore_path}`')
-
-                        if _logger.confirm('Do you want to overwrite it?'):
-                            _logger.warning(f'Deleting existing filestore directory')
-                            shutil.rmtree(filestore_path)
-
-                    shutil.copytree(tmp_filestore_path, filestore_path)
-
-                psql_load(self.database, tmp_sql_path)
+        match ext:
+            case '.dump':
+                pg_restore(self.database, self.dump_path)
+            case '.sql':
+                psql_load(self.database, self.dump_path)
+            case '.gz':
+                cmd = f"zcat {self.dump_path}"
+                psql_pipe(self.database, cmd)
+            case '.zip':
+                self.handle_zipped_dump()
+            case _:
+                raise ValueError(f'Unrecognized extension `{ext}` for file {self.dump_path}')
 
         db_config = self.config['databases']
         db_config.set(self.database, 'version', self.db_version(self.database))
@@ -142,3 +124,51 @@ class RestoreCommand(commands.LocalDatabaseCommand):
             clean.CleanCommand.run_with(**self.args.__dict__)
 
         return 0
+
+    def handle_zipped_dump(self):
+        with ZipFile(self.dump_path, 'r') as zipref:
+            infolist = zipref.infolist()
+            members = {entry.filename for entry in infolist}
+
+            if 'dump.sql' not in members:
+                raise Exception(f'{self.dump_path} contains no `dump.sql`')
+            cmd = f"unzip -p {self.dump_path} dump.sql"
+            psql_pipe(self.database, cmd)
+
+            filestore_re = re.compile(r'^filestore/(.+)$')
+            filestore_infos = [info for info in infolist if filestore_re.match(info.filename)]    
+            if filestore_infos:
+                filestores_root = Path.home() / '.local/share/Odoo/filestore'
+                filestore_path = filestores_root / self.database
+                _logger.info(f'Filestore detected, installing to {filestore_path}')
+
+                if os.path.isdir(filestore_path):
+                    _logger.warning(f'A filestore already exists at `{filestore_path}`')
+
+                    actions = set('OMC')
+                    choice = False
+                    while not choice and choice not in actions:
+                        try:
+                            choice = _logger.ask('[O]verwrite / [M]erge / [C]ancel ').capitalize()[0]
+                        except ValueError:
+                            continue
+                    
+                    match choice:
+                        case 'O':
+                            _logger.warning(f'Deleting existing filestore directory')
+                            shutil.rmtree(filestore_path)
+                        case 'M':
+                            _logger.warning(f'Merging existing filestore directory')
+                        case 'C':
+                            _logger.warning(f'Not touching filestore...finishing')
+                            return
+
+            for zipinfo in filestore_infos:
+                # member is saved w/ full filepath, modify it
+                m = filestore_re.match(zipinfo.filename)
+                zipinfo.filename = m.group(1)
+                # filestore saves using sha256 as filename
+                # if exists, don't extract
+                if (filestore_path / zipinfo.filename).exists():
+                    continue
+                zipref.extract(zipinfo, path=filestore_path)
