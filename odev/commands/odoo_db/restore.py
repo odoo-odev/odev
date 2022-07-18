da74979c956af249fce863d634095eabfd6f5ab3
abk16 <and.theunnamed@gmail.com>
2022-07-18 11:26:14 +0200

    [IMP] restore: progress bar for dump file import
    - use enlighten to display a progress bar of the dump file restoring process
    - use python to open the file to pass as stdin to the restore subprocess command
    - for zipfiles, use `ZipFile.open` to stream uncompressed data directly to subprocess pipe
    - renamed `pg_subprocess` to `restore_subprocess`, refactored signature, renamed decorated functions

--- odev/commands/odoo_db/restore.py
+++ odev/commands/odoo_db/restore.py
@@ -4,10 +4,16 @@ import os
 import re
 import shutil
 import subprocess
+import time
+import zipfile
 from argparse import Namespace
-from functools import wraps
+from functools import partial, wraps
+from io import UnsupportedOperation
 from pathlib import Path
+from typing import IO, Callable, Tuple
 from zipfile import ZipFile
+
+import enlighten
 
 from odev.commands.odoo_db import clean, remove
 from odev.constants import DB_TEMPLATE_SUFFIX
@@ -20,31 +26,79 @@ from odev.utils.signal import capture_signals
 _logger = logging.getLogger(__name__)
 
 
-def pg_subprocess(fnc):
+BUFFER_SIZE = 64 * 1024
+
+BAR_FORMAT = (
+    "{desc}{desc_pad}{percentage:3.0f}%|{bar}| "
+    "{count:!.2j}{unit} / {total:!.2j}{unit} [{elapsed}<{eta}, {rate:!.2j}{unit}/s]"
+)
+
+
+def restore_subprocess(fnc: Callable[..., Tuple[Callable[[], IO], int, str]]):
     @wraps(fnc)
     def wrapper(*args):
         database = args[0]
         _logger.info(f"Importing SQL data to database {database}")
-        commandline = fnc(*args)
-        with capture_signals():
-            subprocess.run(commandline, shell=True, check=True, stdout=subprocess.DEVNULL)
+        file_loader, total_size, commandline = fnc(*args)
+        with file_loader() as fp:
+            try:
+                fp.fileno()  # ok to pass file descriptor
+                stdin = fp
+            except UnsupportedOperation:
+                stdin = subprocess.PIPE
+            # TODO: pipe stdout and show last line in statusbar?
+            with subprocess.Popen(commandline, shell=True, stdin=stdin, stdout=subprocess.DEVNULL) as proc:
+                with enlighten.get_manager(set_scroll=False) as manager, manager.counter(
+                    total=float(total_size),
+                    desc="restoring",
+                    unit="B",
+                    bar_format=BAR_FORMAT,
+                    leave=False,
+                ) as pbar:
+                    last_pos: int = 0
+                    while proc.poll() is None:
+                        if stdin is subprocess.PIPE:
+                            proc.stdin.write(fp.read(BUFFER_SIZE))
+                        else:
+                            time.sleep(0.1)
+                        pos: int = fp.tell()
+                        pbar.update(float(pos - last_pos))
+                        last_pos = pos
+                        if pos >= total_size:
+                            if stdin is subprocess.PIPE:
+                                proc.stdin.close()
+                            break
+                if proc.poll() is None:
+                    _logger.info("Waiting for SQL data import to finish...")
+                    proc.wait()
+                if proc.returncode != 0:
+                    raise subprocess.CalledProcessError(returncode=proc.returncode, cmd=commandline)
 
     return wrapper
 
 
-@pg_subprocess
-def pg_restore(database, dump_path):
-    return f"pg_restore -d {database} {dump_path}"
+def get_dump_loader(dump_path: str) -> Callable[[], IO]:
+    return partial(open, dump_path, "rb", buffering=BUFFER_SIZE)
 
 
-@pg_subprocess
-def psql_load(database, sql_file):
-    return f'psql "{database}" < "{sql_file}"'
+@restore_subprocess
+def pg_restore(database: str, dump_path: str) -> Tuple[Callable[[], IO], int, str]:
+    return get_dump_loader(dump_path), os.path.getsize(dump_path), f'pg_restore -d "{database}"'
 
 
-@pg_subprocess
-def psql_pipe(database, cmd):
-    return f'{cmd} | psql "{database}"'
+@restore_subprocess
+def psql_plain(database: str, sql_file: str) -> Tuple[Callable[[], IO], int, str]:
+    return get_dump_loader(sql_file), os.path.getsize(sql_file), f'psql "{database}"'
+
+
+@restore_subprocess
+def psql_gz(database: str, dump_file: str) -> Tuple[Callable[[], IO], int, str]:
+    return get_dump_loader(dump_file), os.path.getsize(dump_file), f'zcat - | psql "{database}"'
+
+
+@restore_subprocess
+def psql_zip(database: str, dumpzip: zipfile.ZipFile, member: str) -> Tuple[Callable[[], IO], int, str]:
+    return partial(dumpzip.open, member, "r", force_zip64=True), dumpzip.getinfo(member).file_size, f'psql "{database}"'
 
 
 class RestoreCommand(database.DBExistsCommandMixin, commands.TemplateCreateDBCommand):
@@ -96,10 +150,9 @@ class RestoreCommand(database.DBExistsCommandMixin, commands.TemplateCreateDBCom
         if ext == ".dump":
             pg_restore(self.database, self.dump_path)
         elif ext == ".sql":
-            psql_load(self.database, self.dump_path)
+            psql_plain(self.database, self.dump_path)
         elif ext == ".gz":
-            cmd = f"zcat {self.dump_path}"
-            psql_pipe(self.database, cmd)
+            psql_gz(self.database, self.dump_path)
         elif ext == ".zip":
             self.handle_zipped_dump()
         else:
@@ -136,14 +189,13 @@ class RestoreCommand(database.DBExistsCommandMixin, commands.TemplateCreateDBCom
         return 0
 
     def handle_zipped_dump(self):
-        with ZipFile(self.dump_path, "r") as zipref:
-            infolist = zipref.infolist()
+        with ZipFile(self.dump_path, "r") as dumpzip:
+            infolist = dumpzip.infolist()
             members = {entry.filename for entry in infolist}
 
             if "dump.sql" not in members:
                 raise Exception(f"{self.dump_path} contains no `dump.sql`")
-            cmd = f"unzip -p {self.dump_path} dump.sql"
-            psql_pipe(self.database, cmd)
+            psql_zip(self.database, dumpzip, "dump.sql")
 
             filestore_re = re.compile(r"^filestore/(.+)$")
             filestore_infos = [info for info in infolist if filestore_re.match(info.filename)]
@@ -181,4 +233,4 @@ class RestoreCommand(database.DBExistsCommandMixin, commands.TemplateCreateDBCom
                     # if exists, don't extract
                     if (filestore_path / zipinfo.filename).exists():
                         continue
-                    zipref.extract(zipinfo, path=filestore_path)
+                    dumpzip.extract(zipinfo, path=filestore_path)
