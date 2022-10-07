odoo-odev/odev

commit 4b8a5d6d5555c37f1e1959b1cbd84c17f6b778e4

[FIX] clean: MailHog server for Odoo 14 (#235)
As SMTP Authentication was backported to Odoo version 14.0, the cleaning queries
had to be adapted. The `smtp_authentication` was only filled in for versions
greater than 15.0 but now includes versions from 14.0 upward.

Additionally, refactoring the query's contruction to avoid duplicate code.

odev/commands/odoo_db/clean.py
@@ -78,16 +78,16 @@ class CleanCommand(commands.LocalDatabaseCommand):
             f"UPDATE ir_config_parameter SET value='{expiration_date}' WHERE key='database.expiration_date'",
         )
 
-        if version <= Version("14.0"):
-            self.queries.append(
-                """INSERT INTO ir_mail_server (name, smtp_host, smtp_port, smtp_encryption, active)
-                VALUES ('Mailhog', 'localhost', 1025, 'none', 't')""",
-            )
-        else:
-            self.queries.append(
-                """INSERT INTO ir_mail_server (name, smtp_host, smtp_port, smtp_authentication, smtp_encryption, active)
-                VALUES ('Mailhog', 'localhost', 1025, 'login', 'none', 't')""",
-            )
+        # Setup of a MailHog mail server for outgoing emails in a local environment;
+        # To be used with https://github.com/mailhog/MailHog
+        mailhog_columns = "name, smtp_host, smtp_port, smtp_encryption, active"
+        mailhog_values = "'MailHog', 'localhost', 1025, 'none', 't'"
+
+        if version >= Version("14.0"):
+            mailhog_columns += ", smtp_authentication"
+            mailhog_values += ", 'login'"
+
+        self.queries.append(f"INSERT INTO ir_mail_server ({mailhog_columns}) VALUES ({mailhog_values})")
 
     def run(self):
         """

commit 1c68a54976c9d96be4d6b53269561cefbc439f79

[FIX] clean: Reset Mailhog queries for Odoo 14
SMTP Authentication wasn't backported to Odoo version 14.0 so 4b8a5d6d5555c37f1e1959b1cbd84c17f6b778e4
introduced an issue. This commit fixes it.

odev/commands/odoo_db/clean.py
@@ -83,7 +83,7 @@ class CleanCommand(commands.LocalDatabaseCommand):
         mailhog_columns = "name, smtp_host, smtp_port, smtp_encryption, active"
         mailhog_values = "'MailHog', 'localhost', 1025, 'none', 't'"
 
-        if version >= Version("14.0"):
+        if version > Version("14.0"):
             mailhog_columns += ", smtp_authentication"
             mailhog_values += ", 'login'"
 