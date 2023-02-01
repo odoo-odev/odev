b9103457b5765b091d95e578fa5ea0202df8e172
avs <avs@odoo.com>
2023-02-01 23:02:35 +0100

    [IMP] setup.directories: merge directories into one

--- odev/setup/directories.py
+++ odev/setup/directories.py
@@ -25,7 +25,7 @@ def __resolve(path: Union[str, Path]) -> Path:
 
 def __is_empty(path: Path) -> bool:
     """Check if a directory is empty."""
-    return path.exists() and not any(path.iterdir())
+    return not path.exists() or not any(path.iterdir())
 
 
 def __move(old: Path, new: Path) -> None:
@@ -53,7 +53,7 @@ def __ask_dir(message: str, default: str = None, path: Optional[Path] = None) ->
         path = __resolve(prompt.directory(message, default))
 
         if not __is_empty(path):
-            logger.warning(f"Directory {path} is not empty and cannot be overridden")
+            logger.warning(f"Directory {path} is not empty")
 
             if not prompt.confirm("Use this directory without moving files?"):
                 path = None
@@ -69,36 +69,16 @@ def setup(config: Optional[ConfigManager] = None) -> None:
 
     :param config: Configuration manager
     """
-    standard: Optional[Path] = None
-    custom: Optional[Path] = None
+    new_path: Optional[Path] = None
 
-    config_section, config_option_standard, config_option_custom = "paths", "odoo", "dev"
+    config_section, config_option = "paths", "repositories"
 
-    old_standard: Path = __resolve(Path(config.get(config_section, config_option_standard, "~/odoo/versions")))
-    old_custom: Path = __resolve(Path(config.get(config_section, config_option_custom, "~/odoo/dev")))
+    old_path: Path = __resolve(Path(config.get(config_section, config_option, "~/odoo/versions")))
 
-    while standard is None or custom is None:
-        while standard is None:
-            standard = __ask_dir(
-                "Where do you want to keep odoo standard repositories?",
-                old_standard.as_posix(),
-                standard,
-            )
+    while new_path is None:
+        new_path = __ask_dir("Where do you want to keep odoo standard repositories?", old_path.as_posix(), new_path)
 
-        while custom is None:
-            custom = __ask_dir(
-                "Where do you want to keep odoo-ps repositories?",
-                old_custom.as_posix(),
-                custom,
-            )
-
-        if standard == custom:
-            logger.error("Standard and custom directories must be different")
-            standard = custom = None
-
-    __move(old_standard, standard)
-    __move(old_custom, custom)
+    __move(old_path, new_path)
 
     logger.debug("Saving directories to configuration file")
-    config.set(config_section, config_option_standard, standard.as_posix())
-    config.set(config_section, config_option_custom, custom.as_posix())
+    config.set(config_section, config_option, new_path.as_posix())
