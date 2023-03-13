d777351e15b6da658444e23490d1982ff56be146
abk16 <and.theunnamed@gmail.com>
2023-03-13 10:17:49 +0100

    [IMP] run: add ominous warning + delay when running unclean db

--- odev/commands/odoo_bin/run.py
+++ odev/commands/odoo_bin/run.py
@@ -1,9 +1,12 @@
 """Runs a local Odoo database."""
-
+import random
+import time
+from collections import namedtuple
+from typing import List
 
 from odev.commands.odoo_db import create, remove
-from odev.constants import DB_TEMPLATE_SUFFIX
-from odev.exceptions.odoo import RunningOdooDatabase
+from odev.constants import DB_TEMPLATE_SUFFIX, UNCLEAN_DB_WARNING_MSG
+from odev.exceptions import CommandAborted, RunningOdooDatabase
 from odev.structures import commands
 from odev.utils import logging
 
@@ -62,6 +65,9 @@ class RunCommand(commands.TemplateDBCommand, commands.OdooBinMixin):
         if self.db_runs() and self.name == "run":
             raise RunningOdooDatabase(f"Database {self.database} is already running")
 
+        if not self.db_is_neutralized():
+            self.print_ominous_uncleandb_warning_message()
+
         if not self.addons:
             _logger.warning(
                 "No additional addons specified. "
@@ -77,3 +83,48 @@ class RunCommand(commands.TemplateDBCommand, commands.OdooBinMixin):
         )
 
         return 0
+
+    @staticmethod
+    def print_ominous_uncleandb_warning_message():
+        if not logging.interactive:
+            raise RuntimeError("Cannot run an unclean database with odev in non-interactive mode")
+        delay_base: float = 0.01
+        lines: List[str] = UNCLEAN_DB_WARNING_MSG.split("\n")
+        max_rows: int = len(lines)
+        max_cols: int = max(len(line) for line in lines)
+        if logging.term.does_styling and max_cols < logging.term.width:
+            Pixel = namedtuple("Pixel", ["char", "row", "col"])
+            print("\n" * (max_rows + 1) + logging.term.move_up(max_rows), end="")
+            for char, color, delay in (
+                (" ", logging.term.red, 0 * delay_base),
+                ("▀█▄▐▌", logging.term.orangered, 1 * delay_base),
+                ("▓", logging.term.red, 3 * delay_base),
+                ("▒", logging.term.red3, 5 * delay_base),
+                ("░", logging.term.darkred, 10 * delay_base),
+            ):
+                pixels: List[Pixel] = [
+                    Pixel(c, row, col) for row, line in enumerate(lines) for col, c in enumerate(line) if c in char
+                ]
+                random.shuffle(pixels)
+                for c, row, col in pixels:
+                    print(logging.term.move_down(row) + logging.term.move_x(col) + color + c, end="")
+                    print(logging.term.move_up(row) + logging.term.move_x(0), end="", flush=True)
+                    time.sleep(delay / max_rows)
+            print(logging.term.normal + logging.term.move_down(max_rows) + logging.term.move_x(0))
+
+        _logger.warning(
+            (logging.term.bold + logging.term.orangered)
+            + (
+                "WARNING!!! Running a non-neutralized database!\n"
+                "If this database is a dump from production, "
+                "it will communicate with the external world and lead to unintended consequences!\n"
+                "If you are not sure what this means, stop this process now with CTRL-C and run `odev clean` first"
+            )
+            + logging.term.normal
+        )
+        if not _logger.confirm(
+            (logging.term.bold + logging.term.orangered)
+            + "Confirm you have read the above warning, understand the risks, and want to continue"
+            + logging.term.normal
+        ):
+            raise CommandAborted()
