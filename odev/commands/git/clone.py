268ffb06b86582f3d14c76384a3c2b80b7ba8f10
avs-odoo <avs@odoo.com>
2024-08-14 16:56:17 +0200

    [4.3][IMP] code: allow opening repositories without a database
    Open any repository on the local system even if not linked to a local
    database. This is useful for browsing repositories without the need to
    create or restore a database.

--- odev/commands/git/clone.py
+++ odev/commands/git/clone.py
@@ -2,36 +2,22 @@
 
 
 from odev.common import args
-from odev.common.commands import DatabaseCommand
-from odev.common.commands.database import DatabaseType
+from odev.common.commands import DatabaseOrRepositoryCommand
 from odev.common.connectors import GitConnector
-from odev.common.databases import DummyDatabase
 from odev.common.logging import logging
 
 
 logger = logging.getLogger(__name__)
 
 
-class CloneCommand(DatabaseCommand):
+class CloneCommand(DatabaseOrRepositoryCommand):
     """Clone a GitHub repository locally, under the path managed by odev. A database name can be passed instead
     of a repository address to find and clone the repository linked to that database.
     """
 
     _name = "clone"
 
-    repository = args.String(description="Git URL of a repository to clone.", nargs="?")
     branch = args.String(description="Branch to checkout after cloning the repository.")
-
-    _database_arg_required = False
-    _database_exists_required = False
-
-    def infer_database_instance(self) -> DatabaseType:
-        if any(char in self.args.database for char in "@:/"):
-            self.args.repository = self.args.database
-            self.args.database = None
-            return DummyDatabase()
-
-        return super().infer_database_instance()
 
     def run(self):
         if not self.args.database and not self.args.repository:
