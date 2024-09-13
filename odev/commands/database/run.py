5523e005b5bf73fc13e054b3a6f043d471861053
avs-odoo <avs@odoo.com>
2024-09-13 16:33:03 +0200

    [IMP] odev.commands.{run,create}: run and create databases from template
    Externalize template database detection in a new class
    `OdoobinTemplateCommand` that is used by both `run` and `create`.
    
    Rename the `--template` option to `--from-template` in both commands.
    
    Allow the `--from-template` option to be used with a database name
    or with no value to fallback to an existing template of the current
    database.
    
    ---

--- odev/commands/database/run.py
+++ odev/commands/database/run.py
@@ -1,18 +1,15 @@
 """Run an Odoo database locally."""
 
-from typing import Optional
 
-from odev.commands.database.create import TEMPLATE_SUFFIX
 from odev.common import args, progress
-from odev.common.commands import OdoobinCommand
-from odev.common.databases import LocalDatabase
+from odev.common.commands import OdoobinTemplateCommand
 from odev.common.logging import logging
 
 
 logger = logging.getLogger(__name__)
 
 
-class RunCommand(OdoobinCommand):
+class RunCommand(OdoobinTemplateCommand):
     """Run the odoo-bin process for the selected database locally.
     The process is run in a python virtual environment depending on the database's odoo version (as defined
     by the installed `base` module). The command takes care of installing and updating python requirements within
@@ -23,22 +20,14 @@ class RunCommand(OdoobinCommand):
     _name = "run"
 
     from_template = args.String(
-        name="template",
-        aliases=["-t", "--template"],
-        description="Name of an existing PostgreSQL database to copy before running.",
+        description="""Name of an existing PostgreSQL database to copy before running.
+        If passed without a value, search for a template database with the same name as the new database.
+        """
     )
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-
-        if self.args.template:
-            self.template: Optional[LocalDatabase] = LocalDatabase(self.args.template + TEMPLATE_SUFFIX)
-            """Template database to copy."""
-
-            if not self.template.exists:
-                self.template = LocalDatabase(self.args.template)
-        else:
-            self.template = None
+        self.infer_template_instance()
 
     def run(self):
         """Run the odoo-bin process for the selected database locally."""
@@ -48,14 +37,14 @@ class RunCommand(OdoobinCommand):
         if self.odoobin.is_running:
             raise self.error(f"Database {self._database.name!r} is already running")
 
-        if self.template:
-            if not self.template.exists:
-                raise self.error(f"Template database {self.template.name!r} does not exist")
+        if self._template:
+            if not self._template.exists:
+                raise self.error(f"Template database {self._template.name!r} does not exist")
 
             if self._database.exists:
-                with progress.spinner(f"Reset database {self._database.name!r} from template {self.template.name!r}"):
+                with progress.spinner(f"Reset database {self._database.name!r} from template {self._template.name!r}"):
                     self._database.drop()
 
-            self.odev.run_command("create", self._database.name, "--template", self.template.name)
+            self.odev.run_command("create", self._database.name, "--from-template", self._template.name)
 
         self.odoobin.run(args=self.args.odoo_args, progress=self.odoobin_progress)
