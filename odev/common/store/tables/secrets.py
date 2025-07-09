9787619e8384127f64b4fac5f58178640f69e424
brinkflew <avs@avanserv.com>
2025-07-09 13:40:55 +0200

    [FIX] store.secrets: Catch specific exceptions during decryption
    During password decryption, we now catch specific exceptions to handle
    errors more gracefully. Avoid catching bare `Exception` as we want to
    ensure that we only handle known issues and let unexpected exceptions
    propagate.

--- odev/common/store/tables/secrets.py
+++ odev/common/store/tables/secrets.py
@@ -109,8 +109,8 @@ class SecretStore(PostgresTable):
                 logger.debug(f"Failed to decrypt with key {key.name}: {e}")
             except UnicodeDecodeError as e:
                 logger.debug(f"Failed to decode decrypted string with key {key.name}: {e}")
-            except Exception:
-                logger.warning(f"Unexpected error when trying to handle SSH key {key.name}", exc_info=True)
+            except ValueError as e:
+                logger.debug(f"Unexpected error when trying to handle SSH key {key.name}: {e}")
 
         if deciphered is None:
             raise OdevError("Decryption failed, no key could be used")
