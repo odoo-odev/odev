83709529c69a6602a21eaf0bef3c5d51de989649
Sébastien Andreatta <sea@odoo.com>
2025-12-05 10:18:06 +0100

    [REF] Replace dynamic plugin loading with explicit `__all__` exports in common modules.

--- odev/common/connectors/__init__.py
+++ odev/common/connectors/__init__.py
@@ -1,32 +1,17 @@
 """Connectors to external services."""
 
-import pkgutil
-from importlib import import_module
-from pathlib import Path
-from typing import cast
-from inspect import isclass
-
-# --- Common modules -----------------------------------------------------------
 from .base import Connector
 from .git import GitConnector, GitWorktree, Stash
 from .postgres import PostgresConnector
 from .rest import RestConnector
 from .rpc import RpcConnector
 
-
-# --- Plugins ------------------------------------------------------------------
-
-odev_path = Path(__file__).parent.parent.parent
-
-plugins = [path for path in (odev_path / "plugins").glob("*/common/connectors") if path.is_dir()]
-modules = pkgutil.iter_modules([directory.as_posix() for directory in plugins])
-
-for module_info in modules:
-    module_path = cast(str, module_info.module_finder.path).replace(str(odev_path.parent) + "/", "").replace("/", ".")  # type: ignore [union-attr]
-    module = import_module(f"{module_path}.{module_info.name}")
-
-    for attribute in dir(module):
-        obj = getattr(module, attribute)
-
-        if isclass(obj) and issubclass(obj, Connector) and obj is not Connector:
-            globals()[attribute] = obj
+__all__ = [
+    "Connector",
+    "GitConnector",
+    "GitWorktree",
+    "PostgresConnector",
+    "RestConnector",
+    "RpcConnector",
+    "Stash",
+]
