90b47f3b77abf0b7b08f12496a8aff6acb7757a4
brinkflew <avs@avanserv.com>
2025-12-11 13:59:25 +0100

    [ADD] Release channels (stable and beta)
    Allow selection of a release channel for Odev and its plugins, directly
    linked to an existing git branch.
    
    Odev and official plugins will provide both 'main' and 'beta' branches.
    Main (stable) will provide less frequent updates for the benefits of
    less breaking changes. Beta will be used for more frequent updates but
    will potentially break more often.
    
    If repositories are set to track an unsupported branch, they will not
    be switched automatically, this is to prevent loss of features
    for people actively developping on Odev or a plugin.

--- odev/commands/utilities/version.py
+++ odev/commands/utilities/version.py
@@ -1,6 +1,7 @@
 from odev._version import __version__
-from odev.common import string
+from odev.common import args, string
 from odev.common.commands import Command
+from odev.common.connectors.git import GitConnector
 from odev.common.logging import logging
 
 
@@ -13,11 +14,29 @@ class VersionCommand(Command):
     _name = "version"
     _aliases = ["v"]
 
+    show_plugins = args.Flag(aliases=["-p", "--show-plugins"], help="Show plugins versions")
+
     def run(self):
         """Print the current version of the application."""
-        logger.info(
-            f"{self.odev.name.capitalize()} version {string.stylize(self.odev.config.update.version, 'repr.version')}"
-        )
+        name = self.odev.name.capitalize()
+        version = string.stylize(self.odev.config.update.version, "repr.version")
+        channel = string.stylize(f"({self.odev.release})", "color.black")
+        logger.info(f"{name} version {version} {channel}")
 
         if self.odev.config.update.version != __version__:
             logger.warning(f"A newer version is available, consider running '{self.odev.name} update'")
+
+        if self.show_plugins:
+            plugins_list: list[str] = []
+
+            for plugin in sorted(self.odev.plugins, key=lambda plugin: plugin.name):
+                plugin_version = string.stylize(plugin.manifest["version"], "repr.version")
+                plugin_repo = GitConnector(plugin.name)
+                plugin_branch = string.stylize(
+                    f"({plugin_repo.repository.active_branch.name if plugin_repo.repository else '<unknown>'})",
+                    "color.black",
+                )
+                plugins_list.append(f"{plugin.name!r} version {plugin_version} {plugin_branch}")
+
+            logger.info(f"Plugins:\n{string.join_bullet(plugins_list)}")
+            self.console.print()
