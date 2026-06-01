1930585ba1a7f8bf20e73a24f06614577358d3fd
Istvan (isko) <isko@odoo.com>
2026-06-01 11:31:39 +0200

    Use readlink if realpath is unavailable macOS does not have realpath installed by default. New code checks realpath, and if unavailable, tries readlink

--- odev.sh
+++ odev.sh
@@ -3,9 +3,13 @@
 # Run odev using the virtualenv if available, otherwise fallback to system python3
 # for compatibility with older running installations of odev.
 
+resolve_path() {
+    realpath "$1" 2>/dev/null || readlink -f "$1"
+}
+
 for interpreter in ~/.config/odev/venv/bin/python3 /usr/bin/python3; do
     if [ -x "$interpreter" ]; then
-        SCRIPT_PATH=$(readlink -f "$0")
+        SCRIPT_PATH=$(resolve_path "$0")
         exec "$interpreter" "$(dirname "$SCRIPT_PATH")/main.py" "$@"
         exit $?
     fi
